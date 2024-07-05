f7f5a2ce26558ba87c381ee8216cd3185727a98d
korayyalcin1903 <korayyalcin1903@gmail.com>
2024-07-05 23:50:48 +0300

    Update Checkout and Cart page

--- Controllers/Orders.cs
+++ Controllers/Orders.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecommerce.Data.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce;
 
 public class Orders:Controller
 {
+    public readonly EcommerceContext _context;
+    public Orders(EcommerceContext context)
+    {
+        _context = context;
+    }
     public async Task<IActionResult> Cart()
     {
         return View();
@@ -11,6 +18,12 @@ public class Orders:Controller
 
     public async Task<IActionResult> Checkout()
     {
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.Name == User.Identity.Name);
+        if (user == null){
+            return NotFound();
+        } else {
+            ViewBag.User = user;
+        }
         return View();
     }
 }
