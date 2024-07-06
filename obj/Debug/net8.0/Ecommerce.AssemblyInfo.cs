8e40d247203a78b232de1daec594ff750bc8bb78
korayyalcin1903 <korayyalcin1903@gmail.com>
2024-07-06 19:50:19 +0300

    Add Session Extensions

--- obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db8f4adc3dfeb88289520ff2007cb1f26d0f6a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc2f5ba22a3944d7f7089a5e348a45b3402f1c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
