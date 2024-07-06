19467f4fff106bb213b88c5453ec967801d9ee8c
korayyalcin1903 <korayyalcin1903@gmail.com>
2024-07-06 20:16:22 +0300

    Update OrderController

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -4,16 +4,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce;
 
-public class Orders:Controller
+public class OrderController:Controller
 {
     public readonly EcommerceContext _context;
-    public Orders(EcommerceContext context)
+    public OrderController(EcommerceContext context)
     {
         _context = context;
     }
+    public Cart Carts { get; set; }
     public IActionResult Cart()
     {
+        Carts = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Cart(int id)
+    {
+        var product = await _context.Products.FirstOrDefaultAsync(i => i.ProductId == id);
+
+        if(product != null){
+            Carts = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Carts.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Carts);
+        }
+
+        return RedirectToPage("/");
     }
 
     public async Task<IActionResult> Checkout()
